863111fe65eccd6259fc2f7e17ebfde9f2ad3870
prasad2606 <58550063+prasad2606@users.noreply.github.com>
2019-12-12 14:57:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,25 @@
 //}).listen(8080); 
 
 var mysql = require('mysql');
+var http = require('http');
 
-  var con = mysql.createConnection({
-  host: 'custome-mysql.gamification.svc.cluster.local',
-  user: 'xxuser',
-  password: 'welcome1'
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
 });
+
+
+http.createServer(function (req, res) {
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }; 
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.end('Connected!');
+
 });
+
+}).listen(8080);
