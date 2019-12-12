34e15420cd483d10aaae906b1d5daf64243f16fe
prasad2606 <58550063+prasad2606@users.noreply.github.com>
2019-12-12 14:11:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("Hello!! This is Team Atlas Cloud");
+  res.write('Hello! This is Team Atlas Cloud');
    res.end('');
 }).listen(8080); 
