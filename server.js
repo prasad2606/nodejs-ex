2bdb274a01a780ca035558bbf2114f3d52fbc977
prasad2606 <58550063+prasad2606@users.noreply.github.com>
2019-12-12 14:37:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+//var http = require('http');
+
+//http.createServer(function (req, res) {
+//  res.writeHead(200, {'Content-Type': 'text/html'});
+//  res.write('Hello! This is Team Atlas Cloud');
+ //  res.end('');
+//}).listen(8080); 
 var http = require('http');
+var mysql = require('mysql');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello! This is Team Atlas Cloud');
-   res.end('');
-}).listen(8080); 
+  var con = mysql.createConnection({
+  host: 'custome-mysql.gamification.svc.cluster.local',
+  user: 'xxuser',
+  password: 'welcome1'
+});
+
+con.connect(function(err) {
+  if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }  
+  res.end('Connected!');
+});
+  }).listen(8080);
